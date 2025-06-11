ede9e134701f0b3c09391907a9ef3a9474014ec7
MartianKay <kei741t@gmail.com>
2025-06-12 01:31:40 +0900

    inital

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://kay:kay@cluster0.toa2vph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://kay:@cluster0.toa2vph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
